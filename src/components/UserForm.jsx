321ddaf88549fd4069c35d21b73230684bc97683
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-12 14:46:58 -0800

    create UserForm and Card components:

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card";
+
+const UserForm = () => {
+  return (
+    <Card>
+      <div>
+        <label>Name</label>
+        <input></input>
+      </div>
+    </Card>
+  )
+}
+
+export default UserForm;
