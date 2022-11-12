a69f8f7b9df99951690c09e479a165b23aeaa934
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-12 15:24:32 -0800

    add submit handler

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,30 +2,48 @@ import { useState } from "react";
 import Card from "./Card";
 
 const UserForm = () => {
-  const [userName, setUserName] = useState('')
-  const [userAge, setUserAge] = useState('')
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
 
+  const userNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
 
-  const userNameHandler = event => {
-    setUserName(event.target.value)
+  const userAgeHandler = (event) => {
+    setUserAge(event.target.value);
+  };
+
+  const newUserHandler = event => {
+    event.preventDefault()
+    const newUser = {
+      name: userName,
+      age: userAge
+    }
+
+    console.log('newUser', newUser)
+    setUserName('')
+    setUserAge('')
   }
-
-  const userAgeHandler = event => {
-    setUserAge(event.target.value)
-  }
-
   return (
     <Card>
-      <div>
-        <label>Name</label>
-        <input value={userName} onChange={userNameHandler}></input>
-      </div>
-      <div>
-        <label>Age (Years)</label>
-        <input value={userAge} type='number' min='0' onChange={userAgeHandler}></input>
-      </div>
+      <form onSubmit={newUserHandler}>
+        <div>
+          <label>Name</label>
+          <input value={userName} onChange={userNameHandler}></input>
+        </div>
+        <div>
+          <label>Age (Years)</label>
+          <input
+            value={userAge}
+            type="number"
+            min="0"
+            onChange={userAgeHandler}
+          ></input>
+        </div>
+        <button type="submit">Add User</button>
+      </form>
     </Card>
-  )
-}
+  );
+};
 
 export default UserForm;
