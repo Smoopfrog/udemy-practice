7cb296297e9044e791506128a630edf7e592dae6
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-14 14:23:45 -0800

    change map key to math.random

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -3,9 +3,9 @@ import User from "./User";
 import styles from './UsersList.module.css'
 
 const Users = (props) => {
-  const userComponents = props.usersArray.map((user, index) => (
+  const userComponents = props.usersArray.map((user) => (
     <User
-      key={index}
+      key={Math.random().toString()}
       name={user.name}
       age={user.age}
       deleteUser={props.deleteUser}
